88df7c21d0f026152cb5fe6a14ab626e278cd339
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-12 12:37:07 -0300

    fix commit menu navegacion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 function App() {
   return (
-    <div className="bg-gray-800">
-      <h1 className="text-white font-bold text-center py-1">Hello</h1>
+    <div className="w-full bg-gray-700">
+      <div className="container sm bg-slate-300 mx-auto py-1 flex justify-between">
+        <h1 className="text-orange-600 text-4xl font-bold">Hello</h1>
+        <input type="text" className="w-96 px-2" placeholder="busca productos..."></input>
+        <ul className="flex w-96 bg-slate-600 font-bold justify-between text-yellow-200">
+          <li>web</li>
+          <li>web</li>
+          <li>web</li>
+        </ul>
+      </div>
     </div>
   );
 }
