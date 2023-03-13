69830d5b9f55aa49ceb16b214376b2b6e25533e5
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-13 18:57:23 -0300

    fix commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Title from "./components/Title";
 import ContainerList from "./components/ItemListContainer"
+import Modal from "./components/Modal";
 
 function App() {
   return (
@@ -8,22 +9,8 @@ function App() {
     <Navbar></Navbar>
     <Title string="grilla"/>
     <ContainerList content="greatting"/>
+    <Modal />
     <Title string="grilla 2"></Title>
-    <div className="container mx-auto grid grid-cols-1 sm:grid-cols-3 gap-4 my-2">
-      <div className="bg-yellow-400 col-span-2 h-56">A</div>
-      <div className="bg-red-500 h-56">B</div>
-      <div className="bg-green-400 h-56">C</div>
-      <div className="bg-blue-400 col-span-4 h-56">D</div>
-    </div>
-    <h1 className="text-center text-5xl mb-5">Grid column with rows</h1>
-    <div className="container mx-auto grid grid-cols-1 grid-rows-1 sm:grid-cols-4 sm:grid-rows-2 gap-4 my-2">
-      <div className="bg-yellow-400 h-56">A</div>
-      <div className="bg-red-500 h-56">B</div>
-      <div className="bg-green-400 col-span-1 row-span-1 sm:col-span-2 sm:row-span-2 h-56">C</div>
-      <div className="bg-blue-400 h-56">D</div>
-      <div className="bg-blue-400 h-56">D</div>
-    </div>
-    <h1 className="text-center text-5xl mb-5">Grid Movil</h1>
     <div className="container mx-auto grid gap-4 grid-cols-3">
       <div className="h-36 sm:h-56 bg-red-200">A</div>
       <div className="h-36 sm:h-56 bg-red-200 col-span-2 grid grid-cols-2 gap-1">
---
+++ src/components/Modal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Modal() {
+  return (
+    <div className='text-5xl w-full'>Modal</div>
+  )
+}
+
+export default Modal
