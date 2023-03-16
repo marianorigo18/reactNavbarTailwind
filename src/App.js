33e5c9a0dc6b7e8c527f8ef7837de48abada77f2
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-16 09:25:07 -0300

    fix commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Title from "./components/Title";
 import ContainerList from "./components/ItemListContainer"
 import Modal from "./components/Modal";
 
 function App() {
+  const [stateModal1, setStateModal] = useState(false)
+  const [stateModal2, setStateModal2] = useState(false)
+  const [stateModal3, setStateModal3] = useState(false)
   return (
     <>
     <Navbar></Navbar>
     <Title string="products"/>
+    {/*Modal #1*/}
+    <Modal
+      state={stateModal1}
+      change={setStateModal}
+      title='Hello i´m dinamic'
+    >
+    </Modal>
+    {/*Modal #2*/}
+    <Modal
+      state={stateModal2}
+      change={setStateModal2}
+      title='Hello i´m dinamic 2'
+    >
+    </Modal>
+    {/*Modal #3*/}
+    <Modal
+      state={stateModal3}
+      change={setStateModal3}
+      image={'https://fastly.picsum.photos/id/15/2500/1667.jpg?hmac=Lv03D1Y3AsZ9L2tMMC1KQZekBVaQSDc1waqJ54IHvo4'}
+      title='Hello i´m dinamic 2'
+    >
+    </Modal>
     <ContainerList>
     <div className="container mx-auto grid gap-4 grid-cols-3">
-      <div className="h-36 sm:h-56 bg-red-200"></div>
-      <div className="h-36 sm:h-56 bg-red-200 col-span-2 grid grid-cols-2 gap-1">
+      <div className="h-36 sm:h-56 bg-red-200" onClick={() => setStateModal(!stateModal1)}></div>
+      <div className="h-36 sm:h-56 bg-red-300 col-span-2 grid grid-cols-2 gap-1" onClick={() => setStateModal2(!stateModal2)}>
       </div>
-      <div className="h-36 sm:h-56 bg-red-200 col-span-2"></div>
-      <div className="h-36 sm:h-56 bg-red-200"></div>
-      <div className="h-36 sm:h-56 bg-red-200"></div>
-      <div className="h-36 sm:h-56 bg-red-200 col-span-2"></div>
-      <div className="h-36 sm:h-56 bg-red-200 col-span-2"></div>
-      <div className="h-36 sm:h-56 bg-red-200"></div>
+      <div className="h-36 sm:h-56 bg-red-400 col-span-2" onClick={() => setStateModal3(!stateModal3)}></div>
+      <div className="h-36 sm:h-56 bg-red-500"></div>
+      <div className="h-36 sm:h-56 bg-blue-200"></div>
+      <div className="h-36 sm:h-56 bg-red-300 col-span-2"></div>
+      <div className="h-36 sm:h-56 bg-green-100 col-span-2"></div>
+      <div className="h-36 sm:h-56 bg-red-400"></div>
     </div>
     </ContainerList>
     </>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-
-function Modal() {
+function Modal({children, state, change, title = 'Alert', image = 'https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68'}) {
   return (
-    <div className='fixed text-5xl text-indigo-500 w-full h-full bg-white top-0 left-0'>
-      <div>
-        <h1 className='text-6xl'>Hello</h1>
+    <>
+    {state &&
+    <div className='fixed text-5xl text-indigo-500 w-full h-full bg-white top-0 left-0 flex justify-center items-center p-10'>
+      <div className='w-96 min-h-96 bg-slate-700 relative rounded p-3'>
+        <div className='flex items-center justify-between mb-5 pb-5'>
+          <h1 className='text-lg'>{title}</h1>
+        </div>
+          <button className='text-lg absolute right-5 top-2'onClick={() => change(false)}>x</button>
+        <div className='flex flex-col items-center'>
+          <h1 className='text-4xl font-bold mb-2.5'>{children}</h1>
+          <p className='text-xl mb-2.5'>{children}</p>
+          <img className='w-full align-top rounded' src={image} alt='imagen'></img>
+          <button onClick={() => change(false)}>enter</button>
+        </div>
       </div>
     </div>
+    }
+    </>
   )
 }
 
