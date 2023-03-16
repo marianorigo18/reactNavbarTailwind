f6745a2a07deba82534e71daa39054ee074f1853
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-16 09:36:17 -0300

    fix commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,7 @@ function Modal({children, state, change, title = 'Alert', image = 'https://fastl
   return (
     <>
     {state &&
-    <div className='fixed text-5xl text-indigo-500 w-full h-full bg-white top-0 left-0 flex justify-center items-center p-10'>
+    <div className='fixed text-5xl text-indigo-500 w-full h-full bg-white top-0 left-0 flex justify-center items-center p-2'>
       <div className='w-96 min-h-96 bg-slate-700 relative rounded p-3'>
         <div className='flex items-center justify-between mb-5 pb-5'>
           <h1 className='text-lg'>{title}</h1>
@@ -12,7 +12,7 @@ function Modal({children, state, change, title = 'Alert', image = 'https://fastl
           <h1 className='text-4xl font-bold mb-2.5'>{children}</h1>
           <p className='text-xl mb-2.5'>{children}</p>
           <img className='w-full align-top rounded' src={image} alt='imagen'></img>
-          <button onClick={() => change(false)}>enter</button>
+          <button className="text-lg" onClick={() => change(false)}>enter</button>
         </div>
       </div>
     </div>
